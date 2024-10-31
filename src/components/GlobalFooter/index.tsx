83ba67110a11dfc47a72fe587c93cc9a6700edec
buaishuasuanfa <1740465868@qq.com>
2024-10-31 14:48:01 +0800

    全局通用布局，请求配置+自动生成请求

---
+++ src/components/GlobalFooter/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+import './index.css'
+import Link from "next/link";
+/**
+ * 全局底部栏组件
+ * @constructor
+ */
+export default function GlobalFooter() {
+    const currentYear = new Date().getFullYear()
+
+  return (
+    <div
+      className="global-footer"
+    >
+        <div>@ {currentYear} 面试刷题平台</div>
+        <Link href={"https://github.com/buaishuasuanfa/ikunmianshi"} target={"_blank"}>
+            GitHub源码地址
+        </Link>
+    </div>
+  );
+}
---
+++ src/layouts/BasicLayout/index.tsx
@@ -0,0 +1,145 @@
+"use client";
+import {
+  GithubFilled,
+  LogoutOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
+import { ProLayout } from "@ant-design/pro-components";
+import { Dropdown, Input, theme } from "antd";
+import React from "react";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import GlobalFooter from "@/components/GlobalFooter";
+import './index.css';
+import {menus} from "../../../config/menu";
+
+interface SearchInputProps {
+  key?: string;
+}
+
+/**
+ * 通用布局
+ * @param key
+ * @constructor
+ */
+const SearchInput = ({ key }: SearchInputProps) => {
+  const { token } = theme.useToken();
+  return (
+    <div
+      key="SearchOutlined"
+      aria-hidden
+      style={{
+        display: "flex",
+        alignItems: "center",
+        marginInlineEnd: 24,
+      }}
+      onMouseDown={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+    >
+      <Input
+        style={{
+          borderRadius: 4,
+          marginInlineEnd: 12,
+        }}
+        prefix={<SearchOutlined style={{}} />}
+        placeholder="搜索题目"
+        variant="borderless"
+      />
+    </div>
+  );
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function BasicLayout({ children }: Props) {
+  const [pathname] = usePathname();
+
+  return (
+    <div
+      id="bisicLayout"
+      style={{
+        height: "100vh",
+        overflow: "auto",
+      }}
+    >
+      <ProLayout
+        title="ikun面试刷题平台"
+        layout="top"
+        logo={
+          <Image
+            src={"/assets/logo.png"}
+            height={32}
+            width={32}
+            alt={"ikun面试刷题平台"}
+          />
+        }
+        location={{
+          pathname,
+        }}
+        avatarProps={{
+          src: "https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg",
+          size: "small",
+          title: "刘佳伟",
+          render: (props, dom) => {
+            return (
+              <Dropdown
+                menu={{
+                  items: [
+                    {
+                      key: "logout",
+                      icon: <LogoutOutlined />,
+                      label: "退出登录",
+                    },
+                  ],
+                }}
+              >
+                {dom}
+              </Dropdown>
+            );
+          },
+        }}
+        actionsRender={(props) => {
+          if (props.isMobile) return [];
+          return [
+            <SearchInput key="search" />,
+            <a
+              key="GithubFilled"
+              href={"https://github.com/buaishuasuanfa/ikunmianshi"}
+              target={"_blank"}
+            >
+              <GithubFilled />
+            </a>,
+          ];
+        }}
+        headerTitleRender={(logo, title, _) => {
+          return (
+            <a>
+              {logo}
+              {title}
+            </a>
+          );
+        }}
+        onMenuHeaderClick={(e) => console.log(e)}
+        // 导航栏菜单
+        menuDataRender={() => {
+          return menus
+        }}
+        // 导航栏样式
+        menuItemRender={(item, dom) => (
+          <Link href={item.path || "/"}>{dom}</Link>
+        )}
+        // 渲染底部栏
+          footerRender={()=>{
+            return <GlobalFooter/>
+          }}
+      >
+        {children}
+      </ProLayout>
+    </div>
+  );
+}
